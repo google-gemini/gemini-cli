8ae1c8b9aec7d004e0e2901421f4e31480af30d5
ongspxm <ongspxm@gmail.com>
2025-06-27 23:40:37 +0800

    fix file cache

--- packages/core/src/code_assist/oauth2.ts
+++ packages/core/src/code_assist/oauth2.ts
@@ -219,9 +219,7 @@ export function getAvailablePort(): Promise<number> {
 
 async function loadCachedCredentials(client: OAuth2Client): Promise<boolean> {
   try {
-    const keyFile =
-      process.env.GOOGLE_APPLICATION_CREDENTIALS || getCachedCredentialPath();
-
+    const keyFile = getCachedCredentialPath();
     const creds = await fs.readFile(keyFile, 'utf-8');
     client.setCredentials(JSON.parse(creds));
 
@@ -249,7 +247,7 @@ async function cacheCredentials(credentials: Credentials) {
 }
 
 function getCachedCredentialPath(): string {
-  return path.join(os.homedir(), GEMINI_DIR, CREDENTIAL_FILENAME);
+  return process.env.GOOGLE_APPLICATION_CREDENTIALS || path.join(os.homedir(), GEMINI_DIR, CREDENTIAL_FILENAME);
 }
 
 function getGoogleAccountIdCachePath(): string {
