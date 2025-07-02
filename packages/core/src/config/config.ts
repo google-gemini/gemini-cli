360241ed9a2ba14f619df88c8844814a9ae96ea5
Deergha Kulkarni <deerghak@google.com>
2025-07-02 09:49:34 +0000

    Added a Tool to list the code repository indexes in a gcp project

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -27,6 +27,7 @@ import {
   GEMINI_CONFIG_DIR as GEMINI_DIR,
 } from '../tools/memoryTool.js';
 import { WebSearchTool } from '../tools/web-search.js';
+import { ListCRITool } from '../tools/ListCRI.js';
 import { GeminiClient } from '../core/client.js';
 import { FileDiscoveryService } from '../services/fileDiscoveryService.js';
 import { GitService } from '../services/gitService.js';
@@ -499,6 +500,9 @@ export function createToolRegistry(config: Config): Promise<ToolRegistry> {
   registerCoreTool(ShellTool, config);
   registerCoreTool(MemoryTool);
   registerCoreTool(WebSearchTool, config);
+  registerCoreTool(ListCRITool, config);
+
+
   return (async () => {
     await registry.discoverTools();
     return registry;
