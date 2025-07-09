914fc35511005e712907085de772c777f4986756
Brian Ray <raybrian@google.com>
2025-07-09 18:28:57 +0000

    initial commit

--- packages/cli/src/services/CommandService.ts
+++ packages/cli/src/services/CommandService.ts
@@ -8,6 +8,7 @@ import { SlashCommand } from '../ui/commands/types.js';
 import { memoryCommand } from '../ui/commands/memoryCommand.js';
 import { helpCommand } from '../ui/commands/helpCommand.js';
 import { clearCommand } from '../ui/commands/clearCommand.js';
+import { mcpCommand } from '../ui/commands/mcpCommand.js';
 import { authCommand } from '../ui/commands/authCommand.js';
 import { themeCommand } from '../ui/commands/themeCommand.js';
 import { chatCommand } from '../ui/commands/chatCommand.js';
@@ -29,6 +30,7 @@ const loadBuiltInCommands = async (): Promise<SlashCommand[]> => [
   privacyCommand,
   statsCommand,
   themeCommand,
+  mcpCommand,
 ];
 
 export class CommandService {
