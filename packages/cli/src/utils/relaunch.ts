60318ce2df8bcb0dade0d44b687a56736362c9fb
Taylor Mullen <ntaylormullen@google.com>
2025-07-12 13:00:28 -0700

    Cleanup on relaunch

--- packages/cli/src/utils/relaunch.ts
+++ packages/cli/src/utils/relaunch.ts
@@ -6,8 +6,10 @@
 
 import { spawn } from 'child_process';
 import process from 'node:process';
+import { cleanupInk } from './cleanup.js';
 
 export function relaunch() {
+  cleanupInk();
   const child = spawn(process.argv[0], process.argv.slice(1), {
     env: process.env,
     detached: true,
