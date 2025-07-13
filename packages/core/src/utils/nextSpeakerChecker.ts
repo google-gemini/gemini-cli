15dde645e6c3827d1238ad82fcf32e75a8b5da35
Sandy Tao <sandytao520@icloud.com>
2025-07-12 21:20:53 -0700

    Import from config

--- packages/core/src/utils/nextSpeakerChecker.ts
+++ packages/core/src/utils/nextSpeakerChecker.ts
@@ -4,8 +4,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { DEFAULT_GEMINI_FLASH_MODEL } from '@google/gemini-cli-core';
 import { Content, SchemaUnion, Type } from '@google/genai';
+import { DEFAULT_GEMINI_FLASH_MODEL } from '../config/models.js';
 import { GeminiClient } from '../core/client.js';
 import { GeminiChat } from '../core/geminiChat.js';
 import { isFunctionResponse } from './messageInspectors.js';
