8c29e85a33c88169f62f7b04a705e39cc69545c7
Shreya <shreyakeshive@google.com>
2025-07-15 15:40:24 -0400

    update build

--- scripts/build.js
+++ scripts/build.js
@@ -33,6 +33,10 @@ if (!existsSync(join(root, 'node_modules'))) {
 // build all workspaces/packages
 execSync('npm run generate', { stdio: 'inherit', cwd: root });
 execSync('npm run build --workspaces', { stdio: 'inherit', cwd: root });
+execSync('npm run package --workspace packages/vscode-ide-companion', {
+  stdio: 'inherit',
+  cwd: root,
+});
 
 // also build container image if sandboxing is enabled
 // skip (-s) npm install + build since we did that above
