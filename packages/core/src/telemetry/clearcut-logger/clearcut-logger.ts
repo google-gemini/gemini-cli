ae224f9f094c198faf618f6cf4a88d43fec2bf2a
Ravikant Agarwal <ravikantag@google.com>
2025-08-06 12:19:37 +0530

    Fix for git issue 5657 to add lines of code added/removed telemetry

--- packages/core/src/telemetry/clearcut-logger/clearcut-logger.ts
+++ packages/core/src/telemetry/clearcut-logger/clearcut-logger.ts
@@ -376,32 +376,31 @@ export class ClearcutLogger {
       },
     ];
 
-    if (event.metadata && event.metadata.ai_added_lines) {
-      data.concat({
-        gemini_cli_key: EventMetadataKey.GEMINI_CLI_AI_ADDED_LINES,
-        value: JSON.stringify(event.metadata.ai_added_lines),
-      });
-    }
-
-    if (event.metadata && event.metadata.ai_removed_lines) {
-      data.concat({
-        gemini_cli_key: EventMetadataKey.GEMINI_CLI_AI_REMOVED_LINES,
-        value: JSON.stringify(event.metadata.ai_removed_lines),
-      });
-    }
-
-    if (event.metadata && event.metadata.user_added_lines) {
-      data.concat({
-        gemini_cli_key: EventMetadataKey.GEMINI_CLI_USER_ADDED_LINES,
-        value: JSON.stringify(event.metadata.user_added_lines),
-      });
-    }
-
-    if (event.metadata && event.metadata.user_removed_lines) {
-      data.concat({
-        gemini_cli_key: EventMetadataKey.GEMINI_CLI_USER_REMOVED_LINES,
-        value: JSON.stringify(event.metadata.user_removed_lines),
-      });
+    if (event.metadata) {
+      if (event.metadata.ai_added_lines !== undefined) {
+        data.push({
+          gemini_cli_key: EventMetadataKey.GEMINI_CLI_AI_ADDED_LINES,
+          value: JSON.stringify(event.metadata.ai_added_lines),
+        });
+      }
+      if (event.metadata.ai_removed_lines !== undefined) {
+        data.push({
+          gemini_cli_key: EventMetadataKey.GEMINI_CLI_AI_REMOVED_LINES,
+          value: JSON.stringify(event.metadata.ai_removed_lines),
+        });
+      }
+      if (event.metadata.user_added_lines !== undefined) {
+        data.push({
+          gemini_cli_key: EventMetadataKey.GEMINI_CLI_USER_ADDED_LINES,
+          value: JSON.stringify(event.metadata.user_added_lines),
+        });
+      }
+      if (event.metadata.user_removed_lines !== undefined) {
+        data.push({
+          gemini_cli_key: EventMetadataKey.GEMINI_CLI_USER_REMOVED_LINES,
+          value: JSON.stringify(event.metadata.user_removed_lines),
+        });
+      }
     }
 
     const logEvent = this.createLogEvent(tool_call_event_name, data);
