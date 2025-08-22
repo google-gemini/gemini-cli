0fff1f75dfb7a9a5f69b5015c3c8414a9e4a1269
Allen Hutchison <adh@google.com>
2025-08-22 14:18:31 -0700

    Fix merge conflict.

--- packages/cli/src/utils/sandbox.ts
+++ packages/cli/src/utils/sandbox.ts
@@ -201,7 +201,9 @@ export async function start_sandbox(
         process.exit(1);
       }
       const profile = (process.env.SEATBELT_PROFILE ??= 'permissive-open');
-      let profileFile = fileURLToPath(new URL(`sandbox-macos-${profile}.sb`, import.meta.url));
+      let profileFile = fileURLToPath(
+        new URL(`sandbox-macos-${profile}.sb`, import.meta.url),
+      );
       // if profile name is not recognized, then look for file under project settings directory
       if (!BUILTIN_SEATBELT_PROFILES.includes(profile)) {
         profileFile = path.join(
