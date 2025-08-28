ec456d5c1af4cee099864344ea411009bd7d9cfb
Shruti Padamata <shruti.p90@gmail.com>
2025-08-28 12:12:59 -0700

    Remove unnecessary old setting

--- packages/cli/src/ui/hooks/useFolderTrust.ts
+++ packages/cli/src/ui/hooks/useFolderTrust.ts
@@ -28,7 +28,6 @@ export const useFolderTrust = (
 
   useEffect(() => {
     const trusted = isWorkspaceTrusted({
-      folderTrust,
       security: {
         folderTrust: {
           featureEnabled: folderTrustFeature,
