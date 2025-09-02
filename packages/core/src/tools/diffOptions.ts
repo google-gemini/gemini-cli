0b7a2ae4dda95fcb16e7b18a0a1906b761647ae1
Shnatu <snatu@google.com>
2025-09-02 16:33:29 -0700

    update(vars): Update var names to ts-like names

--- packages/core/src/tools/diffOptions.ts
+++ packages/core/src/tools/diffOptions.ts
@@ -19,33 +19,14 @@ export function getDiffStat(
   userStr: string,
 ): DiffStat {
   const getStats = (patch: Diff.ParsedDiff) => {
-<<<<<<< HEAD
-    let added_lines = 0;
-    let removed_lines = 0;
-    let added_chars = 0;
-    let removed_chars = 0;
-=======
     let addedLines = 0;
     let removedLines = 0;
     let addedChars = 0;
     let removedChars = 0;
->>>>>>> aff3525c (feat(telemetry): Add character counts to diff stats)
 
     patch.hunks.forEach((hunk: Diff.Hunk) => {
       hunk.lines.forEach((line: string) => {
         if (line.startsWith('+')) {
-<<<<<<< HEAD
-          added_lines++;
-          added_chars += line.length - 1;
-        } else if (line.startsWith('-')) {
-          removed_lines++;
-          removed_chars += line.length - 1;
-        }
-      });
-    });
-
-    return { added_lines, removed_lines, added_chars, removed_chars };
-=======
           addedLines++;
           addedChars += line.length - 1;
         } else if (line.startsWith('-')) {
@@ -55,10 +36,9 @@ export function getDiffStat(
       });
     });
     return { addedLines, removedLines, addedChars, removedChars };
->>>>>>> aff3525c (feat(telemetry): Add character counts to diff stats)
   };
 
-  const aiPatch = Diff.structuredPatch(
+  const modelPatch = Diff.structuredPatch(
     fileName,
     fileName,
     oldStr,
@@ -67,7 +47,7 @@ export function getDiffStat(
     'Proposed',
     DEFAULT_DIFF_OPTIONS,
   );
-  const modelStats = getStats(aiPatch);
+  const modelStats = getStats(modelPatch);
 
   const userPatch = Diff.structuredPatch(
     fileName,
@@ -81,16 +61,6 @@ export function getDiffStat(
   const userStats = getStats(userPatch);
 
   return {
-<<<<<<< HEAD
-    ai_added_lines: modelStats.added_lines,
-    ai_removed_lines: modelStats.removed_lines,
-    model_added_chars: modelStats.added_chars,
-    model_removed_chars: modelStats.removed_chars,
-    user_added_lines: userStats.added_lines,
-    user_removed_lines: userStats.removed_lines,
-    user_added_chars: userStats.added_chars,
-    user_removed_chars: userStats.removed_chars,
-=======
     ai_added_lines: modelStats.addedLines,
     ai_removed_lines: modelStats.removedLines,
     model_added_chars: modelStats.addedChars,
@@ -99,6 +69,5 @@ export function getDiffStat(
     user_removed_lines: userStats.removedLines,
     user_added_chars: userStats.addedChars,
     user_removed_chars: userStats.removedChars,
->>>>>>> aff3525c (feat(telemetry): Add character counts to diff stats)
   };
 }
