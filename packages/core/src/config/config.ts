1047f2d2757eca06d309ecffb450f86a3eb9b115
silvio junior <silviojr.dcc@gmail.com>
2025-09-09 20:29:49 +0000

    turn off smartEdit by default

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -359,7 +359,7 @@ export class Config {
     this.useRipgrep = params.useRipgrep ?? false;
     this.shouldUseNodePtyShell = params.shouldUseNodePtyShell ?? false;
     this.skipNextSpeakerCheck = params.skipNextSpeakerCheck ?? false;
-    this.useSmartEdit = params.useSmartEdit ?? true;
+    this.useSmartEdit = params.useSmartEdit ?? false;
     this.extensionManagement = params.extensionManagement ?? true;
     this.storage = new Storage(this.targetDir);
     this.enablePromptCompletion = params.enablePromptCompletion ?? false;
