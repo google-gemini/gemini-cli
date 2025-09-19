46bda4c8678d77ea84f847aca50d5aaeea5132d0
Anjali Sridhar <anjsridhar@gmail.com>
2025-09-19 13:20:51 -0700

    wip

--- packages/core/src/tools/write-todos.ts
+++ packages/core/src/tools/write-todos.ts
@@ -15,7 +15,7 @@ import {
 // Insprired by langchain/deepagents.
 export const WRITE_TODOS_DESCRIPTION = `This tool can help you list out the current subtasks that are required to be completed for a given user request. The list of subtasks helps you keep track of the current task, organize complex queries and help ensure that you don't miss any steps. With this list, the user can also see the current progress you are making in executing a given task.
 
-Depending on the task complexity, you should first divide a given task in subtasks and then use this tool to list out the subtasks that are required to be completed for a given user request.
+Depending on the task complexity, you should first divide a given task into subtasks and then use this tool to list out the subtasks that are required to be completed for a given user request.
 Each of the subtasks should be clear and distinct. 
 
 Use this tool for complex queries that require multiple steps. If you find that the request is actually complex after you have started executing the user task, create a todo list and use it. If execution of the user task requires multiple steps, planning and generally is higher complexity than a simple Q&A, use this tool.
@@ -26,17 +26,17 @@ DO NOT use this tool for simple tasks that can be completed in less than 2 steps
 
 - pending: Work has not begun on a given subtask.
 - in_progress: Marked just prior to beginning work on a given subtask. You should only have one subtask as in_progress at a time.
-- completed: Subtask was succesfully completed with no errors or issues. If the subtask required more more steps to complete, update the todo list. If more information is required from the user and you are in interactive mode, add that as a new subtask for request more information. All steps should be identified as completed only when they are completed.
-- cancelled: As you update the todo list, some tasks are not required anymore due to the dynamic nature of the task. In this case, mark the subtasks as cancelled so that the user still sees them in the list and knows that they are not required anymore.
+- completed: Subtask was succesfully completed with no errors or issues. If the subtask required more steps to complete, update the todo list with the subtasks. All steps should be identified as completed only when they are completed.
+- cancelled: As you update the todo list, some tasks are not required anymore due to the dynamic nature of the task. In this case, mark the subtasks as cancelled.
 
 
 ## Methodology for using this tool
-1. Use this too as soon as you receive a user request based on the complexity of the task.
-2. Keep track of every subtask that you take my update this list
+1. Use this todo list list as soon as you receive a user request based on the complexity of the task.
+2. Keep track of every subtask that you update the list with.
 3. Mark a subtask as in_progress before you begin working on it. You should only have one subtask as in_progress at a time.
-4. Update the subtask list as you proceed in executing the task. The subtask list is not static and should reflect the current progress you are making.
+4. Update the subtask list as you proceed in executing the task. The subtask list is not static and should reflect your progress and current plans, which may evolve as you acquire new information.
 5. Mark a subtask as completed when you have completed it.
-6. Mark a subtask as cancelled if you update the todo list as you proceed in executing the task.
+6. Mark a subtask as cancelled if the subtask is no longer needed.
 7. You must update the todo list as soon as you start, stop or cancel a subtask. Don't batch or wait to update the todo list.
 
 
@@ -59,7 +59,7 @@ ToDo list created by the agent:
 The agent used the todo list to break the task into distinct, manageable steps:
 1. Building an entire interactive web application from scratch is a highly complex, multi-stage process involving setup, UI development, logic integration, and deployment.
 2. The agent inferred the core functionality required for a "logo creator," such as UI controls for customization (Task 3) and an export feature (Task 7), which must be tracked as distinct goals.
-3. The agent rightly inferred the requirement of a API service model for interacting with the image model endpoint.
+3. The agent rightly inferred the requirement of an API service model for interacting with the image model endpoint.
 </reasoning>
 </example>
 
