ca75c0e3cff99ae51a302ca00f587c8b338ba329
Alessio <alessio.attilio@engineer.com>
2025-10-02 10:56:40 +0200

    Clean up eslint.config.js by removing comments
    Removed commented-out code for obtaining __dirname.

--- eslint.config.js
+++ eslint.config.js
@@ -14,16 +14,6 @@ import vitest from '@vitest/eslint-plugin';
 import globals from 'globals';
 import licenseHeader from 'eslint-plugin-license-header';
 
-/*
-  ESM way to get __dirname
-
-  const __filename = url.fileURLToPath(import.meta.url);
-  const __dirname = path.dirname(__filename);
-
-  Determine the monorepo root (assuming eslint.config.js is at the root)
-  const projectRoot = __dirname;
-*/
-
 export default tseslint.config(
   {
     // Global ignores
