415a350996f4c1773ea345cab35c02fcc7e44d11
Maneesh Tewani <mtewani@google.com>
2025-10-02 20:56:22 -0700

    Removed todo comment

--- packages/cli/src/config/extension.ts
+++ packages/cli/src/config/extension.ts
@@ -496,7 +496,6 @@ export async function installExtension(
           `Extension "${newExtensionName}" is already installed. Please uninstall it first.`,
         );
       }
-      // TODO: only request consent if -y isn't passed in
       await maybeRequestConsentOrFail(
         newExtensionConfig,
         requestConsent,
