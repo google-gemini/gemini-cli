bff2f73c061eac1e73b25a9a4f529232b505364f
Shreya <shreyakeshive@google.com>
2025-10-10 11:41:41 -0700

    add comment

--- packages/cli/src/ui/AppContainer.tsx
+++ packages/cli/src/ui/AppContainer.tsx
@@ -209,7 +209,9 @@ export const AppContainer = (props: AppContainerProps) => {
   const branchName = useGitBranchName(config.getTargetDir());
 
   // Layout measurements
+  // For performance profiling only
   const mainControlsRef = useRef<DOMElement>(null);
+  // this is for profiling only
   const rootUiRef = useRef<DOMElement>(null);
   const originalTitleRef = useRef(
     computeWindowTitle(basename(config.getTargetDir())),
