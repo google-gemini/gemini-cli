5a40b1805e59dd8ce410ba94a8b53def29e2aaaa
Eric Rahm <erahm@google.com>
2025-10-13 20:15:20 +0000

    refactor(core): remove legacy ignore parsers
    Removes `GeminiIgnoreParser` and `GitIgnoreParser` and their associated tests.
    These parsers have been replaced by `IgnoreFileManager` in `FileDiscoveryService`.
    Also removes the export of `gitIgnoreParser.js` from `packages/core/src/index.ts`.
    
    Issue: #5259

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -37,7 +37,6 @@ export * from './utils/schemaValidator.js';
 export * from './utils/errors.js';
 export * from './utils/getFolderStructure.js';
 export * from './utils/memoryDiscovery.js';
-export * from './utils/gitIgnoreParser.js';
 export * from './utils/gitUtils.js';
 export * from './utils/editor.js';
 export * from './utils/quotaErrorDetection.js';
