1b47376a1acbe003cab53ec38ead270509127a67
Abhi <abhipatel@google.com>
2025-10-21 13:36:46 -0400

    Revert "Disable Routing by default (#11549)"
    This reverts commit 8aace3aff2720e73405eff715678fa4b68b4aea6.

--- packages/cli/src/config/config.test.ts
+++ packages/cli/src/config/config.test.ts
@@ -1328,7 +1328,7 @@ describe('loadCliConfig model selection', () => {
       argv,
     );
 
-    expect(config.getModel()).toBe(DEFAULT_GEMINI_MODEL);
+    expect(config.getModel()).toBe('auto');
   });
 
   it('always prefers model from argv', async () => {
@@ -1641,12 +1641,12 @@ describe('loadCliConfig useRipgrep', () => {
   });
 
   describe('loadCliConfig useModelRouter', () => {
-    it('should be false by default when useModelRouter is not set in settings', async () => {
+    it('should be true by default when useModelRouter is not set in settings', async () => {
       process.argv = ['node', 'script.js'];
       const argv = await parseArguments({} as Settings);
       const settings: Settings = {};
       const config = await loadCliConfig(settings, [], 'test-session', argv);
-      expect(config.getUseModelRouter()).toBe(false);
+      expect(config.getUseModelRouter()).toBe(true);
     });
 
     it('should be true when useModelRouter is set to true in settings', async () => {
