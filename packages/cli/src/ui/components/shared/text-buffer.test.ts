8610a9d044b98d316ebc098cc811933afd66f727
kuishou68 <lijianlin6868@gmail.com>
2025-12-14 11:35:38 +0800

    fix: Restore spelling errors in secon.

--- packages/cli/src/ui/components/shared/text-buffer.test.ts
+++ packages/cli/src/ui/components/shared/text-buffer.test.ts
@@ -780,7 +780,7 @@ describe('useTextBuffer', () => {
       expect(result.current.allVisualLines).toEqual([
         'line',
         'one',
-        'second',
+        'secon',
         'd',
         'line',
       ]);
