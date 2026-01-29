9b34303745d13a82e8f221e3858317b510897350
Alisa Novikova <62909685+alisa-alisa@users.noreply.github.com>
2026-01-29 11:00:19 -0800

    wrapping the write call in a if (process.stdout.isTTY) check to ensure these codes are only sent when in an interactive terminal

--- integration-tests/globalSetup.ts
+++ integration-tests/globalSetup.ts
@@ -73,9 +73,11 @@ export async function setup() {
 
 export async function teardown() {
   // Disable mouse tracking
-  process.stdout.write(
-    '\x1b[?1000l\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?1002l',
-  );
+  if (process.stdout.isTTY) {
+    process.stdout.write(
+      '\x1b[?1000l\x1b[?1003l\x1b[?1015l\x1b[?1006l\x1b[?1002l',
+    );
+  }
 
   // Cleanup the test run directory unless KEEP_OUTPUT is set
   if (process.env['KEEP_OUTPUT'] !== 'true' && runDir) {
