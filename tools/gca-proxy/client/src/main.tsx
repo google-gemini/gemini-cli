118430fecb6ea5c220e61f40c8d74e8e85e154b7
Gaurav Ghosh <gaghosh@google.com>
2026-02-09 14:08:57 -0800

    feat: add GCA API interactive proxy tool with a web UI for intercepting and editing responses.

---
+++ tools/gca-proxy/client/src/main.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * Copyright 2026 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
