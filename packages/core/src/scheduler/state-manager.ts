e8cac3627c09ebfcf2d1ca0358286f18fa7c34bc
mkorwel <matt.korwel@gmail.com>
2026-02-20 19:12:14 +0000

    chore: revert unrelated formatting changes in scheduler and compression

--- packages/core/src/scheduler/state-manager.ts
+++ packages/core/src/scheduler/state-manager.ts
@@ -17,7 +17,8 @@ import type {
   ExecutingToolCall,
   ToolCallResponseInfo,
 } from './types.js';
-import { CoreToolCallStatus , ROOT_SCHEDULER_ID } from './types.js';
+import { CoreToolCallStatus } from './types.js';
+import { ROOT_SCHEDULER_ID } from './types.js';
 import type {
   ToolConfirmationOutcome,
   ToolResultDisplay,
