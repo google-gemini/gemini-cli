56d631903539c2ff33a2c76857290da47fd9bca6
christine betts <chrstn@uw.edu>
2026-03-09 19:31:31 -0400

    Add support for updating extension sources and names (#21715)

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -363,6 +363,10 @@ export interface GeminiCLIExtension {
      */
     directory?: string;
   };
+  /**
+   * Used to migrate an extension to a new repository source.
+   */
+  migratedTo?: string;
 }
 
 export interface ExtensionInstallMetadata {
