fd5bc3b8334a92a9909e3627cb3428e87b76eb47
Andrea Alberti <a.alberti82@gmail.com>
2026-03-11 08:08:00 +0100

    refactor: read openEditorInNewWindow directly via useSettings in text-buffer

--- packages/cli/src/ui/AppContainer.tsx
+++ packages/cli/src/ui/AppContainer.tsx
@@ -568,11 +568,6 @@ export const AppContainer = (props: AppContainerProps) => {
     return isValidEditorType(val) ? val : undefined;
   }, [settings.merged.general.preferredEditor]);
 
-  const getOpenEditorInNewWindow = useCallback(
-    () => settings.merged.general.openEditorInNewWindow,
-    [settings.merged.general.openEditorInNewWindow],
-  );
-
   const buffer = useTextBuffer({
     initialText: '',
     viewport: { height: 10, width: inputWidth },
@@ -581,7 +576,6 @@ export const AppContainer = (props: AppContainerProps) => {
     escapePastedPaths: true,
     shellModeActive,
     getPreferredEditor,
-    getOpenEditorInNewWindow,
   });
   const bufferRef = useRef(buffer);
   useEffect(() => {
