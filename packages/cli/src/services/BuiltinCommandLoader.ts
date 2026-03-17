9f0e833199b50f8255bf232beeabc1904233df27
Furkan Köykıran <furkankoykiran@gmail.com>
2026-03-17 05:19:07 +0300

    feat(cli): register fork command in builtin loader
    Registers the new fork command in BuiltinCommandLoader to make it
    available as a built-in slash command.

--- packages/cli/src/services/BuiltinCommandLoader.ts
+++ packages/cli/src/services/BuiltinCommandLoader.ts
@@ -49,6 +49,7 @@ import { policiesCommand } from '../ui/commands/policiesCommand.js';
 import { privacyCommand } from '../ui/commands/privacyCommand.js';
 import { profileCommand } from '../ui/commands/profileCommand.js';
 import { quitCommand } from '../ui/commands/quitCommand.js';
+import { forkCommand } from '../ui/commands/forkCommand.js';
 import { restoreCommand } from '../ui/commands/restoreCommand.js';
 import { resumeCommand } from '../ui/commands/resumeCommand.js';
 import { statsCommand } from '../ui/commands/statsCommand.js';
@@ -154,6 +155,7 @@ export class BuiltinCommandLoader implements ICommandLoader {
             },
           ]
         : [extensionsCommand(this.config?.getEnableExtensionReloading())]),
+      forkCommand,
       helpCommand,
       footerCommand,
       shortcutsCommand,
