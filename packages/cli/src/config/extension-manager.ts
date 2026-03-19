b2fe426d90e0b8da95d0b659922f7ce6593de440
mkorwel <matt.korwel@gmail.com>
2026-03-19 16:44:15 -0700

    revert: move extension manager fixes to separate branch

--- packages/cli/src/config/extension-manager.ts
+++ packages/cli/src/config/extension-manager.ts
@@ -248,7 +248,7 @@ export class ExtensionManager extends ExtensionLoader {
       ) {
         tempDir = await ExtensionStorage.createTmpDir();
         const parsedGithubParts = tryParseGithubUrl(installMetadata.source);
-        if (!parsedGithubParts || installMetadata.ref) {
+        if (!parsedGithubParts) {
           await cloneFromGit(installMetadata, tempDir);
           installMetadata.type = 'git';
         } else {
@@ -1284,9 +1284,7 @@ export async function inferInstallMetadata(
     source.startsWith('http://') ||
     source.startsWith('https://') ||
     source.startsWith('git@') ||
-    source.startsWith('sso://') ||
-    source.startsWith('github:') ||
-    source.startsWith('gitlab:')
+    source.startsWith('sso://')
   ) {
     return {
       source,
