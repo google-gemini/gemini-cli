8ec892e5b2bbf8165d5aa55841f855e2ca56dac8
Anushka Sarkar <anushkasarkar792@gmail.com>
2026-03-27 12:51:00 +0530

    test: fix vitest environment setup and reduce act warnings

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -3,7 +3,17 @@
  * Copyright 2026 Google LLC
  * SPDX-License-Identifier: Apache-2.0
  */
+import { beforeAll, beforeEach, afterEach, vi } from 'vitest';
+import { setMaxListeners } from 'node:events';
+
 let originalConsole: typeof console;
+
+beforeAll(() => {
+  (
+    globalThis as unknown as { IS_REACT_ACT_ENVIRONMENT: boolean }
+  ).IS_REACT_ACT_ENVIRONMENT = true;
+  setMaxListeners(50);
+});
 
 beforeEach(() => {
   originalConsole = { ...console };
