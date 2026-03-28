2aa49060e469a78245093d4f0693854f754558e9
Daniel Elliott <ssfdre38@msn.com>
2026-03-28 16:00:24 -0700

    security: prevent PowerShell injection in completion script

--- packages/cli/src/utils/completion-powershell.ts
+++ packages/cli/src/utils/completion-powershell.ts
@@ -9,18 +9,19 @@
  * This script uses the built-in yargs --get-yargs-completions support.
  */
 export function getPowerShellCompletionScript(binName: string): string {
+  const escapedBinName = binName.replace(/'/g, "''");
   return `
 function GeminiCompletion {
     param($commandName, $wordToComplete, $cursorPosition)
     $completionArgs = @("--get-yargs-completions")
     $completionArgs += $wordToComplete
-    $completions = & ${binName} $completionArgs
+    $completions = & '${escapedBinName}' $completionArgs
     $completions | ForEach-Object {
         [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
     }
 }
 
-Register-ArgumentCompleter -CommandName '${binName}' -ScriptBlock {
+Register-ArgumentCompleter -CommandName '${escapedBinName}' -ScriptBlock {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
     GeminiCompletion -commandName $commandName -wordToComplete $wordToComplete
 }
