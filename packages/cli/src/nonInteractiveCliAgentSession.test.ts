7553ee65c2fa49bddacc832aa443dda063da1da6
Adam Weidman <adamfweidman@google.com>
2026-04-02 12:49:20 -0700

    test(cli): fix unused _error variable in catch block

--- packages/cli/src/nonInteractiveCliAgentSession.test.ts
+++ packages/cli/src/nonInteractiveCliAgentSession.test.ts
@@ -1882,7 +1882,7 @@ describe('runNonInteractive', () => {
           input,
           prompt_id: promptId,
         });
-      } catch (_error) {
+      } catch {
         // Expected exit
       }
 
