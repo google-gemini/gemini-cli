1373fd9daacaa11d63c84f4f28574ba65d9602d8
Your Name <joshualitt@google.com>
2026-04-21 16:06:10 +0000

    spec fix for base llm client

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -1697,11 +1697,6 @@ export class Config implements McpContext, AgentLoopContext {
     return this.baseLlmClient;
   }
 
-  async ensureExperimentsAndGetBaseLlmClient(): Promise<BaseLlmClient> {
-    await this.ensureExperimentsLoaded();
-    return this.getBaseLlmClient();
-  }
-
   getLocalLiteRtLmClient(): LocalLiteRtLmClient {
     if (!this.localLiteRtLmClient) {
       this.localLiteRtLmClient = new LocalLiteRtLmClient(this);
