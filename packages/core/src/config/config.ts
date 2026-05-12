390f89e250b49a9999b960860e1ed36569eb7920
Adam Weidman <adamfweidman@google.com>
2026-05-12 17:59:05 -0400

    chore(core): remove redundant getAgentSessionSubagentEnabled method

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -2586,10 +2586,6 @@ export class Config implements McpContext, AgentLoopContext {
     return this.contextManagement;
   }
 
-  getAgentSessionSubagentEnabled(): boolean {
-    return this.agentSessionSubagentEnabled;
-  }
-
   getMemoryBoundaryMarkers(): readonly string[] {
     return this.memoryBoundaryMarkers;
   }
