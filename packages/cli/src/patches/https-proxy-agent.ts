0b967a0d47515e30a95c41a6a377d6b925c3665d
Suryansh Pandey <suryanshp991@gmail.com>
2026-06-27 13:59:59 +0530

    fix(agent): prevent silent scope expansion on task failure
    Fixes #28155

--- packages/cli/src/patches/https-proxy-agent.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * Copyright 2026 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-// eslint-disable-next-line import/no-relative-packages
-export { HttpsProxyAgent } from '../../../../node_modules/https-proxy-agent/dist/index.js';
